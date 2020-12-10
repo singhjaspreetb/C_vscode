9191090c070b44b996700ca6c45b7dbb6018fcf3
Jaspreet Singh <harshitbhattal786@gmail.com>
2020-12-10 23:36:34 +0530

    str

---
+++ Unit 2/str1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+int main()
+{
+    int choice;
+    char str[100];
+    printf("1: scanf()\t2: gets()\t3: getchar()\t4: sscanf()\nSelect the string input method: ");
+    scanf("%d",&choice);
+    printf("Enter the String: ");
+    switch (choice)
+    {
+    case 1:
+        fflush(stdin);
+        scanf("%[^\n]%*c",str);
+        break;
+    case 2:
+        fflush(stdin);
+        gets(str);
+        break;
+    case 3:
+        fflush(stdin);
+        getchar();
+        break;
+    case 4:
+        fflush(stdin);
+        sscanf("");
+        break;
+    default:
+        printf("You make wrong choice!");
+        break;
+    }
+    printf("1: printf()\t2: puts()\t3: putchar()\t4: sprintf()\nSelect the string output method: ");
+    scanf("%d",&choice);
+    switch (choice)
+    {
+        case 1:
+        printf("String is: %s",str);
+        break;
+        case 2:
+        puts(str);
+        break;
+        case 3:
+        putchar();
+        break;
+        case 4:
+        sprintf("");
+        break;
+        default:
+        printf("You make wrong choice!");
+        break;
+    }
+    return 0;
+}
---
+++ Unit 2/str2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[100];
+    int choice,len=0;
+    printf("Enter the string: ");
+    gets(str);
+    printf("1: By Predefined function\t2: By Manually\nSelect the method for count the length of entred string: ");
+    scanf("%d",&choice);
+    switch (choice)
+    {
+    case 1:
+        len=strlen(str);
+        break;
+    case 2:
+        while(str[len]1='\0')
+        {
+            len++;
+        }
+        break;
+    default:
+        printf("You make worng choice!");
+        break;
+    }
+    printf("Length of string is: %d",len);
+    return 0;
+}
